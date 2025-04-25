5ea51c03b35ccf7f8d940fc93ed3ab95fb500a6b
Nazar Lipkovskyy <nazar.lipkovskyy@spacelabs.com>
2025-04-25 16:35:27 +0300

    Split monolithic app into microservices and refactor POM structure
    Reorganized the project by splitting the monolithic application into three microservices: Accounts, Loans, and Cards. Each microservice has dedicated configuration, source code, and tests. Also refactored the parent POM to better manage dependencies and plugin configurations across all services.

--- accounts/src/main/java/com/nazjara/AccountsApplication.java
+++ accounts/src/main/java/com/nazjara/AccountsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMicroservicesApplication {
+public class AccountsApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringBootMicroservicesApplication.class, args);
+    SpringApplication.run(AccountsApplication.class, args);
   }
 
 }
